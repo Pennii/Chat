0d53f3809339d52f6af963bbccdadc2e91d919c5
Pennii <matiasspennino@gmail.com>
2025-01-14 19:14:29 +0100

    chat funcional

---
+++ mensaje.php
@@ -0,0 +1,21 @@
+<?php
+$mensajesEscritos = file_get_contents('chat.txt');
+if (trim($mensajesEscritos) === "") {
+    $mensajesEscritos = "<p>Bienvenido al chat, recuerda comportarte</p>";
+}
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $json = file_get_contents('php://input');
+    $datos = json_decode($json, true);
+    if (filter_has_var(INPUT_COOKIE, "usuario")) {
+        $usuario = filter_input(INPUT_COOKIE, "usuario");
+        $mensaje = htmlspecialchars($datos["mensaje"]);
+        $mensaje = "<p>" . $usuario . ": " . $datos["mensaje"] . " " . date('d-m-Y H:i:s') . "</p>";
+    }else{
+        $mensaje = "ERROR NO HAY USUARIO";
+    }
+    $chat = $mensajesEscritos . $mensaje;
+} else {
+    $chat = $mensajesEscritos;
+}
+file_put_contents('chat.txt', $chat);
+echo json_encode(["chat" => $chat]);
